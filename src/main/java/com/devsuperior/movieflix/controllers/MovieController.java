fda09b0f68a53012963abf74e41153e4e1107ad8
Cristiano Araujo <crisd.silva75@gmail.com>
2025-08-11 20:59:28 -0300

    GenreControllerIT Ok and Half MovieControllerIT

---
+++ src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -0,0 +1,27 @@
+package com.devsuperior.movieflix.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
+import com.devsuperior.movieflix.services.MovieService;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MovieController {
+
+	@Autowired
+	private MovieService service;
+
+	@PreAuthorize("hasAnyRole('ROLE_VISITOR','ROLE_MEMBER')")
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<MovieDetailsDTO> findById(@PathVariable Long id) {
+		MovieDetailsDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+}
